f06f805c6addf05703fcf2604fb5fb071847106d
mrkaplan0 <101001725+mrkaplan0@users.noreply.github.com>
2025-05-30 13:37:23 +0200

    status type enum created.

--- src/main/java/com/winston/crm_mit_oemer/App.java
+++ src/main/java/com/winston/crm_mit_oemer/App.java
@@ -2,20 +2,37 @@ package com.winston.crm_mit_oemer;
 
 import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.stage.Stage;
 
 import java.io.IOException;
 
 public class App extends Application {
+
+    private static Scene scene;
+
     @Override
     public void start(Stage stage) throws IOException {
-        FXMLLoader fxmlLoader = new FXMLLoader(App.class.getResource("hello-view.fxml"));
-        Scene scene = new Scene(fxmlLoader.load());
+
+        // Baut die Objekte nach der Vorgabe der FXML-Datei
+        scene = new Scene(loadFXML("main-view"));
         stage.setTitle("Hello!");
         stage.setScene(scene);
         stage.show();
     }
+
+    // Überschreibt die sichtbare View
+    static void setRoot(String fxml) throws IOException {
+        scene.setRoot   (loadFXML(fxml));
+    }
+
+    // Lädt die FXML-Datei und baut daraus die GUI
+    private static Parent loadFXML(String fxml) throws IOException {
+        FXMLLoader fxmlLoader = new FXMLLoader(App.class.getResource(fxml + ".fxml"));
+        return  fxmlLoader.load();
+    }
+
 
     public static void main(String[] args) {
         launch();
