40c83b67045644b5b7ca1dbbc78262c7930e3059
mrkaplan0 <101001725+mrkaplan0@users.noreply.github.com>
2025-05-30 15:30:49 +0200

    style: add main page with style

---
+++ src/main/java/com/winston/crm_mit_oemer/controller/MainController.java
@@ -0,0 +1,43 @@
+package com.winston.crm_mit_oemer.controller;
+
+import com.winston.crm_mit_oemer.App;
+import javafx.fxml.FXML;
+import javafx.fxml.Initializable;
+import javafx.scene.control.Button;
+import javafx.scene.control.Label;
+
+import java.io.IOException;
+import java.net.URL;
+import java.util.ResourceBundle;
+
+public class MainController implements Initializable {
+
+
+    @FXML
+    private Label welcomeText;
+
+    @FXML
+    private Button personalButton;
+
+    @Override
+    public void initialize(URL url, ResourceBundle resourceBundle) {
+        //TODO: if user is standard user, make button invisible
+        //personalButton.setVisible(false);
+    }
+    @FXML
+    protected void onKundeManagementClicked() throws IOException {
+        App.setRoot("main-view");
+
+    }
+    @FXML
+    protected void onTaskManagementClicked() throws IOException {
+        App.setRoot("main-view");
+
+    }
+
+    @FXML
+    protected void onPersonalManagementClicked() throws IOException {
+        App.setRoot("main-view");
+
+    }
+}
